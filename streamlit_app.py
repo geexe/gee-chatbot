1b84155ac96dec12624e56a8643437f6159d1e78
geexe <somaticmutationtypeforza@gmail.com>
2024-09-28 04:33:23 +0000

    chatbot added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import google.generativeai as genai
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+for message in st.session_state.chat_history:
+    with st.chat_message("User"):
+        st.markdown(message)
+
+st.title("🎈 My new Chatbot app")
+if prompt := st.text_input("You: ", placeholder="Type your msg"):
+    st.session_state.chat_history.append(prompt)
+    st.chat_message("User").markdown(prompt)
+
+for message in st.session_state.chat_history:
+    st.write(message)
